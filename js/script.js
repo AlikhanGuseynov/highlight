fcd3448afed1e52aeb9ea2fc2fe126a586144da2
Name-test <alixanh@goldenpay.az>
2021-11-16 20:05:16 +0400

    all in footer & header

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
 $('.header-search-btn').click(() => {
     $('.header-search-input').toggleClass('active')
 })
+
+$('.sidebar-menu-toggle-btn').click(() => {
+    $('.sidebar-menu').toggleClass('active')
+})
+
+
+$('.sidebar-nav-link').click((e) => {
+    $(e.target).siblings('.sidebar-submenu').toggleClass('active');
+})
+
