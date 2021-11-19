4c3366a6ed095c48586d75475afb189e080151d6
Name-test <alixanh@goldenpay.az>
2021-11-19 20:11:55 +0400

    blog-detail

--- js/script.js
+++ js/script.js
@@ -6,8 +6,41 @@ $('.sidebar-menu-toggle-btn').click(() => {
     $('.sidebar-menu').toggleClass('active')
 })
 
-
 $('.sidebar-nav-link').click((e) => {
     $(e.target).siblings('.sidebar-submenu').toggleClass('active');
 })
 
+$(document).ready(function () {
+
+
+    const blogDetailCarousel = $(".blog-detail-carousel")
+    if ($(window).width() > 700) {
+        blogDetailCarousel.removeClass('owl-carousel')
+    } else {
+        blogDetailCarousel.owlCarousel({
+            dots: true,
+            margin: 10,
+            nav: true,
+            items: 1,
+        });
+    }
+
+    $('.close-carousel-btn').click((e) => {
+        $(".carousel-popup-wrapper").removeClass('active')
+    })
+
+    $('.carousel-item').click((e) => {
+        $(".carousel-popup-wrapper").addClass('active');
+        const asd = $(".popup-carousel")
+        asd.owlCarousel({
+            dots: false,
+            margin: 10,
+            nav: true,
+            items: 1,
+        });
+        // asd.trigger('open.owl.carousel', 2);
+
+    })
+
+
+})
